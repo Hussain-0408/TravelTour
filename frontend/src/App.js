0b3a808ed5b729544d7fca6d6ff05d85b87382c0
Hussain-0408 <ihatealthaf0408@gmail.com>
2025-04-17 09:54:48 +0530

    create Component folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './Components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login';
+import Contactus from './Components/Contactus';
+import Register from './Components/Register';
+import Aboutus from './Components/Aboutus'
+import OurServices from './Components/OurServices';
+import TourServices from './Components/OurServices';
+import Gallary from './Components/Gallery';
+import TrikkingTour from './Components/TrikkingTour'
+import Logout from './Components/Logout'
+import Forgotpassword from './Components/Forgotpassword';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+      <Routes>
+      
+        <Route path='/' element= {<Home/>} />
+        <Route path='/Login' element= {<Login/>} />
+        <Route path='/contactus' element= {<Contactus/>} />
+        <Route path='/register' element= {<Register/>} />
+        <Route path = '/Aboutus' element = {<Aboutus/>} />
+        <Route path = '/Gallery'  element = {<Gallary/>} />
+        <Route path = '/OurServices'  element = {<OurServices/>} />
+        <Route path = '/TourServices'  element = {<TourServices/>} />
+        <Route path = "/trikkingtour" element = {<TrikkingTour/>} />
+       <Route path = "/Login" element = {<Logout/>} />
+       <Route path = "/forgotpassword" element={<Forgotpassword />} />
+       
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
