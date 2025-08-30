bb021ac6d910098e2fc900980fb9728341beab57
Hussain <althafhusssaian@gmail.com>
2025-08-30 09:54:04 +0530

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
 import Home from './Components/Home';
-import { BrowserRouter,  Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
 import Contactus from './Components/Contactus';
 import Register from './Components/Register';
 import Aboutus from './Components/Aboutus';
 import OurServices from './Components/OurServices';
-// import TourServices from './Components/OurServices';
 import Tourpackages from './Components/Tourpackages';
 import Gallary from './Components/Gallery';
 import Logout from './Components/Logout'
@@ -19,19 +18,19 @@ function App() {
   return (
     <div >
       <BrowserRouter>
-      <Routes>   
-        <Route path='/' element= {<Home/>} />
-        <Route path='/Login' element= {<Login/>} />
-        <Route path='/contactus' element= {<Contactus/>} />
-        <Route path='/Register' element= {<Register/>} />
-        <Route path = '/Aboutus' element = {<Aboutus/>} />
-        <Route path = '/Gallery'  element = {<Gallary/>} />
-        <Route path = '/OurServices'  element = {<OurServices/>} />
-        < Route path ='/Tourpackages' element = {<Tourpackages/>} />
-       <Route path = "/Logout" element = {<Logout/>} />
-       <Route path = "/forgotpassword" element={<Forgotpassword />} />
-       <Route path = "/BookingForm" element={<BookingForm/>} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/contactus' element={<Contactus />} />
+          <Route path='/Register' element={<Register />} />
+          <Route path='/Aboutus' element={<Aboutus />} />
+          <Route path='/Gallery' element={<Gallary />} />
+          <Route path='/OurServices' element={<OurServices />} />
+          < Route path='/Tourpackages' element={<Tourpackages />} />
+          <Route path="/Logout" element={<Logout />} />
+          <Route path="/forgotpassword" element={<Forgotpassword />} />
+          <Route path="/BookingForm" element={<BookingForm />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
